fbf6018819f65e45a790f03c44a33ece414c7e2d
Anas <anzagha@mbc.net>
2025-09-10 13:30:17 +0300

    Finished week 1 Tasks

--- src/main/java/PaymentSystem/Payment.java
+++ src/main/java/PaymentSystem/Payment.java
@@ -2,6 +2,13 @@ package PaymentSystem;
 
 import java.math.BigDecimal;
 import java.util.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 
 public class Payment {
     private final String id;
@@ -15,31 +22,5 @@ public class Payment {
         this.currency = currency;
         this.status = PaymentStatus.PENDING;
     }
-
-    public String getId() {
-        return id;
-    }
-    public BigDecimal getAmount() {
-        return amount;
-    }
-    public String getCurrency() {
-        return currency;
-    }
-    public PaymentStatus getStatus() {
-        return status;
-    }
-    public void setStatus(PaymentStatus status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString(){
-        return "Payment { Id: " + id +
-                ", Amount: " + amount +
-                ", Currency: " + currency +
-                ", Status: " + status + "}";
-    }
-
-
 
 }
